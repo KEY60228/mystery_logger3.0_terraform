ea72a8b7d6d8c0441fe237bb7348be31a67f38a7
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-04-29 22:58:42 +0900

    add ecr repository

--- src/ecr.tf
+++ src/ecr.tf
@@ -1,11 +1,41 @@
 # ECRレポジトリ
-resource "aws_ecr_repository" "nazolog_ecr" {
-    name = "nazolog-ecr"
+resource "aws_ecr_repository" "nazolog_ecr_php" {
+    name = "nazolog-ecr-php"
 }
 
 # ライフサイクルポリシー
-resource "aws_ecr_lifecycle_policy" "nazolog_ecr_policy" {
-    repository = aws_ecr_repository.nazolog_ecr.name
+resource "aws_ecr_lifecycle_policy" "nazolog_ecr_php_policy" {
+    repository = aws_ecr_repository.nazolog_ecr_php.name
+
+    policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 release tagged images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["release"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+# ECRレポジトリ
+resource "aws_ecr_repository" "nazolog_ecr_nginx" {
+    name = "nazolog-ecr-nginx"
+}
+
+# ライフサイクルポリシー
+resource "aws_ecr_lifecycle_policy" "nazolog_ecr_nginx_policy" {
+    repository = aws_ecr_repository.nazolog_ecr_nginx.name
 
     policy = <<EOF
 {
